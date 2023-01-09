62774b9d73165ba59fa24be8bf83cfee15521d3f
Dave Estey <daveestey.working@gmail.com>
2023-01-09 03:28:16 -0400

    just need to add more questions and have them be directed properly

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
 
-  # undefined
+  # f
+
+  ## Table of Contents
+  [Description](https://github.com/DaveEstey/README-Generator#description)
+  [Installation](https://github.com/DaveEstey/README-Generator#installation)
+  [Usage](https://github.com/DaveEstey/README-Generator#usage)
+  [License](https://github.com/DaveEstey/README-Generator#license)
+  [Badges](https://github.com/DaveEstey/README-Generator#badges)
   
   ## Description
   
-  My project is about me.
+  g
   
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
-
   ## Installation
   
   undefined
   
   ## Usage
-  
+ 
+  undefined
+
   ## License
+  
+  ![License: IPL 1.0](https://img.shields.io/badge/License-MIT-blue.svg)]undefined
 
   ## Badges
   
+  undefined
+
   ## How to Contribute
-    
+  
+  undefined
+
   ## Tests
   
+  d
+
   ## Questions
+  
+  githun
+  dave g
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if (license != null){
     const badge = `![License: IPL 1.0](https://img.shields.io/badge/License-${license}-blue.svg)]`
     return badge;
-  } else return null;
+  } else return "";
 
   
 }
@@ -15,12 +15,26 @@ function renderLicenseLink(license) {
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+const section = `${renderLicenseBadge(license)}${renderLicenseLink(license)}`
 
+  if (license == null) return "";
+else {
+  return section;
+
+} 
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {//Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
   return `
   # ${data.name}
+
+  ## Table of Contents
+  [Description](https://github.com/DaveEstey/README-Generator#description)
+  [Installation](https://github.com/DaveEstey/README-Generator#installation)
+  [Usage](https://github.com/DaveEstey/README-Generator#usage)
+  [License](https://github.com/DaveEstey/README-Generator#license)
+  [Badges](https://github.com/DaveEstey/README-Generator#badges)
   
   ## Description
   
@@ -31,16 +45,29 @@ function generateMarkdown(data) {//Description, Table of Contents, Installation,
   ${data.installation}
   
   ## Usage
-  
+ 
+  ${data.installation}
+
   ## License
+  
+  ${renderLicenseSection(data.license)}
 
   ## Badges
   
+  ${data.installation}
+
   ## How to Contribute
-    
+  
+  ${data.installation}
+
   ## Tests
   
+  ${data.test}
+
   ## Questions
+  
+  ${data.github}
+  ${data.email}
 `;
 }
 
